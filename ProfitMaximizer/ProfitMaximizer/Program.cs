3e7ad814266535a942bcbb9503e2b57f4785999c
Mahmut SAZLIK <mahmutsazlik@users.noreply.github.com>
2021-04-03 23:09:33 +0300

    HW01 deki çözüm eklendi.

--- ProfitMaximizer/ProfitMaximizer/Program.cs
+++ ProfitMaximizer/ProfitMaximizer/Program.cs
@@ -6,7 +6,29 @@ namespace ProfitMaximizer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] stations = { 3, -5, 2, 11, -8, 9, -5 };
+            int value = findMostProfitable(stations);
+            Console.WriteLine(value);
+            Console.ReadKey();
+        }
+
+        private static int findMostProfitable(int[] stations)
+        {
+            int most = int.MinValue;
+            int sum = 0;
+            for (int i = 0; i < stations.Length - 1; i++)
+            {
+                sum = stations[i];
+                for (int j = i + 1; j < stations.Length; j++)
+                {
+                    sum += stations[j];
+                    if (sum > most)
+                    {
+                        most = sum;
+                    }
+                }
+            }
+            return most;
         }
     }
 }
